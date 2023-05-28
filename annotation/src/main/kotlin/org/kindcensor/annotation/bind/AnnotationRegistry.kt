6715dc02d099a5007327aed32f8108fe55a5900d
xander27 <xander27b@gmail.com>
2023-05-28 15:24:39 +0700

    Comments

--- annotation/src/main/kotlin/org/kindcensor/annotation/bind/AnnotationRegistry.kt
+++ annotation/src/main/kotlin/org/kindcensor/annotation/bind/AnnotationRegistry.kt
@@ -8,8 +8,14 @@ import org.kindcensor.annotation.ToStringMaskEnding
 import org.kindcensor.core.DataMasker
 import kotlin.reflect.KClass
 
+/**
+ * Registry of annotations. Hold information about annotations and connects it implementing functions
+ */
 object AnnotationRegistry {
 
+    /**
+     * List of all registered annotations
+     */
     val all: List<KClass<out Annotation>> = listOf(
         ToStringHide::class,
         ToStringInitial::class,
@@ -19,10 +25,19 @@ object AnnotationRegistry {
         ToStringMaskEnding::class
     )
 
+    /**
+     * Map of all registered annotations associated by names
+     */
     val bySimpleNames: Map<String, KClass<out Annotation>> = all.associateBy {
         it.simpleName ?: error("Failed to get simpleName for $it")
     }
 
+    /**
+     * Execute logic specified in annotation for on string representation of a given object
+     * @param annotation The annotation to apply
+     * @param subject The subject to apply logic
+     * @return String representation of subject processed by annotation logic or null if no suitable method was found
+     */
     fun apply(annotation: Annotation, subject: Any?): String? = when (annotation) {
         is ToStringHide -> DataMasker.hide(subject, annotation.preserveLength, annotation.preserveNull, annotation.mask)
         is ToStringInitial -> DataMasker.initial(subject, annotation.withPeriodEnding)
--- annotation/src/main/kotlin/org/kindcensor/annotation/bind/Binding.kt
+++ annotation/src/main/kotlin/org/kindcensor/annotation/bind/Binding.kt
@@ -1,5 +1,10 @@
 package org.kindcensor.annotation.bind
 
+/**
+ * The interface for function that generates string by input object
+ * Purely for code generations simplification
+ */
+@Suppress("unused")
 fun interface Binding {
 
     fun process(subject: Any?): String?
